5541fe45ddc726522d590a4ec10f4213d781b996
Filip Rychnavsky <FRychnavsky@cvs.de>
2020-03-09 14:22:00 +0100

    add Button "Connect und TextFeld m_rTextBoxResult

---
+++ Demos/graph-tutorial_FR/Demo_MS_Graph_SDK/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Demo_MS_Graph_SDK
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.m_rButtonConnect = new System.Windows.Forms.Button();
+			this.m_rTextBoxResult = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// m_rButtonConnect
+			// 
+			this.m_rButtonConnect.BackColor = System.Drawing.Color.PaleGreen;
+			this.m_rButtonConnect.Location = new System.Drawing.Point(30, 15);
+			this.m_rButtonConnect.Name = "m_rButtonConnect";
+			this.m_rButtonConnect.Size = new System.Drawing.Size(118, 48);
+			this.m_rButtonConnect.TabIndex = 0;
+			this.m_rButtonConnect.Text = "Connect";
+			this.m_rButtonConnect.UseVisualStyleBackColor = false;
+			// 
+			// m_rTextBoxResult
+			// 
+			this.m_rTextBoxResult.Location = new System.Drawing.Point(464, 30);
+			this.m_rTextBoxResult.Multiline = true;
+			this.m_rTextBoxResult.Name = "m_rTextBoxResult";
+			this.m_rTextBoxResult.Size = new System.Drawing.Size(268, 265);
+			this.m_rTextBoxResult.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.m_rTextBoxResult);
+			this.Controls.Add(this.m_rButtonConnect);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button m_rButtonConnect;
+		private System.Windows.Forms.TextBox m_rTextBoxResult;
+	}
+}
+
