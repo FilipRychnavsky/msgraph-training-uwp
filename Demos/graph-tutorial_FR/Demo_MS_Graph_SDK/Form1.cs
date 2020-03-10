bc44de26eea433c95ff678da6f093842cf191dae
Filip Rychnavsky <FRychnavsky@cvs.de>
2020-03-10 15:01:33 +0100

    OAuth.resx

--- Demos/graph-tutorial_FR/Demo_MS_Graph_SDK/Form1.cs
+++ Demos/graph-tutorial_FR/Demo_MS_Graph_SDK/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Microsoft.Graph;
 using Microsoft.Graph.Auth;
 using Microsoft.Identity.Client;
+using System.Resources;
 
 namespace Demo_MS_Graph_SDK
 {
@@ -22,12 +23,22 @@ namespace Demo_MS_Graph_SDK
 
 		private void m_rButtonConnect_Click(object sender, EventArgs e)
 		{
+			const string resxFile = @"..\graph-tutorial\OAuth.resw";
+			// statt ResourceLoader von UWP benutze ich C# App Resoucen
+			ResXResourceSet oauthSettings = new ResXResourceSet(resxFile);
 			// Build a client application.
+			var appId = oauthSettings.GetString("AppId");
 			IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
-									.Create("INSERT-CLIENT-APP-ID")
+									.Create(appId)
 									.Build();
 			// Create an authentication provider by passing in a client application and graph scopes.
-			DeviceCodeProvider authProvider = new DeviceCodeProvider(publicClientApplication, graphScopes);
+			// Load OAuth settings
+			//var oauthSettings = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("OAuth");
+			string sScopes = oauthSettings.GetString("Scopes");
+			//var graphScopes = oauthSettings.GetString("Scopes");
+			System.Collections.Generic.IEnumerable<string> rIEnumerableGraphScopes = new System.Collections.Generic.List<string>();
+			rIEnumerableGraphScopes.Append(sScopes);
+			DeviceCodeProvider authProvider = new DeviceCodeProvider(publicClientApplication, rIEnumerableGraphScopes);
 			// Create a new instance of GraphServiceClient with the authentication provider.
 			GraphServiceClient graphClient = new GraphServiceClient(authProvider);
 		}
